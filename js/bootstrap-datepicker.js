77c01da869149d101378695b9173886864eda014
Bogdan Dumitru <dumbogdan@gmail.com>
2014-09-18 14:29:48 +0300

    UMD wrap + component.json

--- js/bootstrap-datepicker.js
+++ js/bootstrap-datepicker.js
@@ -20,7 +20,18 @@
  * limitations under the License.
  * ========================================================= */
 
-(function($, undefined){
+(function (factory) {
+    if (typeof define === 'function' && define.amd) {
+        // AMD. Register as an anonymous module.
+        define(['jquery'], factory);
+    } else if (typeof exports === 'object') {
+        // Node/CommonJS
+        factory(require('jquery'));
+    } else {
+        // Browser globals
+        factory(jQuery);
+    }
+}(function($, undefined){
 
 	var $window = $(window);
 
@@ -1668,4 +1679,4 @@
 		$('[data-provide="datepicker-inline"]').datepicker();
 	});
 
-}(window.jQuery));
+}));
